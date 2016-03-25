bee1ad37b36dca8ac346693fe1c9f19d980a6900
julienasp <julien.aspirot@usherbrooke.ca>
2016-03-25 17:01:53 -0400

    maj interface

--- StartPoint.java
+++ StartPoint.java
@@ -12,12 +12,16 @@ public class StartPoint {
     private static final Logger logger = Logger.getLogger(StartPoint.class);    
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);                
+		Scanner sc = new Scanner(System.in);
+                System.out.println("Veuillez choisir entre 1 ou 2 | 1-DNS Maison 2-DNS Java");
+                
+                String choixDNS = sc.nextLine();
+                
                 System.out.println("Veuillez saisir le URL complet du site internet que vous souhaitez récupérer ? Ex. http://stackoverflow.com/questions or http://ici.radio-canada.ca/techno");             
                 
                 String choixUser = sc.nextLine();
                 
-                HTTPClient myHTTPClient = new HTTPClient();
+                HTTPClient myHTTPClient = new HTTPClient(choixDNS);
                 
                 if( myHTTPClient.enableConnection(choixUser) == 1 ){
                     logger.info("StartPoint: La connexion a été établie");
